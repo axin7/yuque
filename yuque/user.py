6ddde0a3fcac3b7f7abefdc5a16c423e0f686f78
axin7 <963600436@qq.com>
2019-07-13 01:16:58 +0800

    feat: 增加获取用户信息 1. 获取单个用户信息 2. 获取认证用户的个人信息

---
+++ yuque/user.py
@@ -0,0 +1,22 @@
+from .base import Base
+
+
+class User(Base):
+    """
+    获取用户信息
+    """
+    _userInfoUrl = '/users/'      # 获取单个用户信息   注：需在最后加 用户id或login      
+    _userInfoAuthUrl = '/user'   # 获取认证的用户的个人信息  
+
+    def info(self):
+        """获取单个用户信息"""
+        url = self._baseUrl + self._userInfoUrl + self.id
+        r = self.get(url=url)
+        return r.json()
+            
+    def info_auth(self):
+        """获取认证用户的个人信息"""
+        url = self._baseUrl + self._userInfoAuthUrl
+        r = self.get(url=url)
+        return r.json()
+
