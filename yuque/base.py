572bd22cd931500d8a9279f50b96d7eb2e8d566d
axin7 <963600436@qq.com>
2019-07-13 03:16:01 +0800

    style: 修改请求头和请求方法

--- yuque/base.py
+++ yuque/base.py
@@ -8,24 +8,23 @@ class Base(object):
     
     :param token: 用户token
     :param id: 用户的 login 或 id
-    :param name: 仓库的 namespace或id
+    :param namespace: 仓库的 namespace或id
     """
     # 基本路径
     _baseUrl = 'https://www.yuque.com/api/v2'    
 
-    # 请求头
-    _headers = {
-        'Content-Type' : 'application/x-www-form-urlencoded',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.46 Safari/537.36'
-    }
+    def __init__(self,token=None,id=None,namespace=None):
+        self.id = id.strip()
+        self.namespace = namespace.strip()
+        self._headers = {
+            'Content-Type' : 'application/x-www-form-urlencoded',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.46 Safari/537.36'
+        }
 
-    _get = partial(requests.get,headers=_headers)
-    _post = partial(requests.post,headers=_headers)
-    _put = partial(requests.put,headers=_headers)
-    _delete = partial(requests.delete,headers=_headers)
-
-    def __init__(self,token=None,id=None,name=None):
         if token:
-            self._headers.update({'X-Auth-Token':token})
-        self.id = id
-        self.name = name
+            self._headers.update({'X-Auth-Token':token.strip()})
+
+        self._get = partial(requests.get,headers=self._headers)
+        self._post = partial(requests.post,headers=self._headers)
+        self._put = partial(requests.put,headers=self._headers)
+        self._delete = partial(requests.delete,headers=self._headers)
